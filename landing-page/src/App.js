b7f1bdeaadf514787f2b18274f6687bb612ed670
vanessaMora702 <vanessamora702@gmail.com>
2020-09-02 21:43:00 -0400

    Added google fonts

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import LandingPage from './LandingPage/landingPage';
+import LandingPage from './LandingPage/landingPage'
 function App() {
   return (
     <div>
--- landing-page/src/LandingPage/landingPage.js
+++ landing-page/src/LandingPage/landingPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Navigationbar from './Navbar/navbar'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../index.css'
+
 function LandingPage() {
     return (
       <div>
